d75a0bbde3aa17f9a0b7b059c606e9b1e96c658d
Oscar Pepper <oscar.pepper1@gmail.com>
2026-02-14 04:46:39 +0000

    remove zebra from dependencies and remove unecessary modules

--- zingo_common_components/src/protocol.rs
+++ zingo_common_components/src/protocol.rs
@@ -1,1 +1,11 @@
-pub mod activation_heights;
+pub struct ActivationHeights {
+    overwinter: Option<u32>,
+    sapling: Option<u32>,
+    blossom: Option<u32>,
+    heartwood: Option<u32>,
+    canopy: Option<u32>,
+    nu5: Option<u32>,
+    nu6: Option<u32>,
+    nu6_1: Option<u32>,
+    nu7: Option<u32>,
+}
