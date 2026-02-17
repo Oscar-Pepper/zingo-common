bc9ba0f9661390c962677165429993ba25a0a4b8
Oscar Pepper <oscar.pepper1@gmail.com>
2026-02-17 05:12:47 +0000

    replace zebra type with new ActivationHeights type which has a more stable public API

--- zingo_common_components/src/protocol.rs
+++ zingo_common_components/src/protocol.rs
@@ -1,4 +1,7 @@
+//! Module for types associated with the zcash protocol and consensus.
+
 /// Network upgrade activation heights for custom testnet and regtest network configuration.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct ActivationHeights {
     overwinter: Option<u32>,
     sapling: Option<u32>,
@@ -13,71 +16,24 @@ pub struct ActivationHeights {
 
 impl Default for ActivationHeights {
     fn default() -> Self {
-        Self::new(
-            Some(1),
-            Some(1),
-            Some(1),
-            Some(1),
-            Some(1),
-            Some(1),
-            Some(1),
-            Some(1),
-            None,
-        )
+        Self::builder()
+            .set_overwinter(Some(1))
+            .set_sapling(Some(1))
+            .set_blossom(Some(1))
+            .set_heartwood(Some(1))
+            .set_canopy(Some(1))
+            .set_nu5(Some(1))
+            .set_nu6(Some(1))
+            .set_nu6_1(Some(1))
+            .set_nu7(None)
+            .build()
     }
 }
 
 impl ActivationHeights {
-    /// Constructor with assertions to ensure all earlier network upgrades are active with an activation height equal
-    /// to or lower than the later network upgrades.
-    #[allow(clippy::too_many_arguments)]
-    pub fn new(
-        overwinter: Option<u32>,
-        sapling: Option<u32>,
-        blossom: Option<u32>,
-        heartwood: Option<u32>,
-        canopy: Option<u32>,
-        nu5: Option<u32>,
-        nu6: Option<u32>,
-        nu6_1: Option<u32>,
-        nu7: Option<u32>,
-    ) -> Self {
-        if let Some(b) = sapling {
-            assert!(overwinter.is_some_and(|a| a <= b));
-        }
-        if let Some(b) = blossom {
-            assert!(sapling.is_some_and(|a| a <= b));
-        }
-        if let Some(b) = heartwood {
-            assert!(blossom.is_some_and(|a| a <= b));
-        }
-        if let Some(b) = canopy {
-            assert!(heartwood.is_some_and(|a| a <= b));
-        }
-        if let Some(b) = nu5 {
-            assert!(canopy.is_some_and(|a| a <= b));
-        }
-        if let Some(b) = nu6 {
-            assert!(nu5.is_some_and(|a| a <= b));
-        }
-        if let Some(b) = nu6_1 {
-            assert!(nu6.is_some_and(|a| a <= b));
-        }
-        if let Some(b) = nu7 {
-            assert!(nu6_1.is_some_and(|a| a <= b));
-        }
-
-        Self {
-            overwinter,
-            sapling,
-            blossom,
-            heartwood,
-            canopy,
-            nu5,
-            nu6,
-            nu6_1,
-            nu7,
-        }
+    /// Constructs new builder.
+    pub fn builder() -> ActivationHeightsBuilder {
+        ActivationHeightsBuilder::new()
     }
 
     /// Returns overwinter network upgrade activation height.
@@ -123,5 +79,145 @@ impl ActivationHeights {
     /// Returns nu7 network upgrade activation height.
     pub fn nu7(&self) -> Option<u32> {
         self.nu7
+    }
+}
+
+/// Leverages a builder method to avoid new network upgrades from causing breaking changes to the public API.
+pub struct ActivationHeightsBuilder {
+    overwinter: Option<u32>,
+    sapling: Option<u32>,
+    blossom: Option<u32>,
+    heartwood: Option<u32>,
+    canopy: Option<u32>,
+    nu5: Option<u32>,
+    nu6: Option<u32>,
+    nu6_1: Option<u32>,
+    nu7: Option<u32>,
+}
+
+impl Default for ActivationHeightsBuilder {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl ActivationHeightsBuilder {
+    /// Constructs a builder with all fields set to `None`.
+    pub fn new() -> Self {
+        Self {
+            overwinter: None,
+            sapling: None,
+            blossom: None,
+            heartwood: None,
+            canopy: None,
+            nu5: None,
+            nu6: None,
+            nu6_1: None,
+            nu7: None,
+        }
+    }
+
+    /// Set `overwinter` field.
+    pub fn set_overwinter(mut self, height: Option<u32>) -> Self {
+        self.overwinter = height;
+
+        self
+    }
+
+    /// Set `sapling` field.
+    pub fn set_sapling(mut self, height: Option<u32>) -> Self {
+        self.sapling = height;
+
+        self
+    }
+
+    /// Set `blossom` field.
+    pub fn set_blossom(mut self, height: Option<u32>) -> Self {
+        self.blossom = height;
+
+        self
+    }
+
+    /// Set `heartwood` field.
+    pub fn set_heartwood(mut self, height: Option<u32>) -> Self {
+        self.heartwood = height;
+
+        self
+    }
+
+    /// Set `canopy` field.
+    pub fn set_canopy(mut self, height: Option<u32>) -> Self {
+        self.canopy = height;
+
+        self
+    }
+
+    /// Set `nu5` field.
+    pub fn set_nu5(mut self, height: Option<u32>) -> Self {
+        self.nu5 = height;
+
+        self
+    }
+
+    /// Set `nu6` field.
+    pub fn set_nu6(mut self, height: Option<u32>) -> Self {
+        self.nu6 = height;
+
+        self
+    }
+
+    /// Set `nu6_1` field.
+    pub fn set_nu6_1(mut self, height: Option<u32>) -> Self {
+        self.nu6_1 = height;
+
+        self
+    }
+
+    /// Set `nu7` field.
+    pub fn set_nu7(mut self, height: Option<u32>) -> Self {
+        self.nu7 = height;
+
+        self
+    }
+
+    /// Builds `ActivationHeights` with assertions to ensure all earlier network upgrades are active with an activation
+    /// height equal to or lower than the later network upgrades.
+    pub fn build(self) -> ActivationHeights {
+        if let Some(b) = self.sapling {
+            assert!(self.overwinter.is_some_and(|a| a <= b));
+        }
+        if let Some(b) = self.blossom {
+            assert!(self.sapling.is_some_and(|a| a <= b));
+        }
+        if let Some(b) = self.heartwood {
+            assert!(self.blossom.is_some_and(|a| a <= b));
+        }
+        if let Some(b) = self.canopy {
+            assert!(self.heartwood.is_some_and(|a| a <= b));
+        }
+        if let Some(b) = self.nu5 {
+            assert!(self.canopy.is_some_and(|a| a <= b));
+        }
+        if let Some(b) = self.nu6 {
+            assert!(self.nu5.is_some_and(|a| a <= b));
+        }
+        if let Some(b) = self.nu6_1 {
+            assert!(self.nu6.is_some_and(|a| a <= b));
+        }
+        if let Some(b) = self.nu7 {
+            assert!(self.nu6_1.is_some_and(|a| a <= b));
+        }
+
+        ActivationHeights {
+            overwinter: self.overwinter,
+            sapling: self.sapling,
+            blossom: self.blossom,
+            heartwood: self.heartwood,
+            canopy: self.canopy,
+            nu5: self.nu5,
+            nu6: self.nu6,
+            nu6_1: self.nu6_1,
+            nu7: self.nu7,
+        }
     }
 }
